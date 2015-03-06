49d4154a49ca95d6f5f5830e6a070498bcf78da0
Gowri Shankar Ravindran <gravindran@xactlycorp.com>
2015-03-06 22:21:22 +0530

    Tech Trendz commit

--- src/main/java/com/example/application/ApplicationStart.java
+++ src/main/java/com/example/application/ApplicationStart.java
@@ -3,9 +3,11 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.example")
+@EnableMongoRepositories(basePackages = "com.example.repository")
 public class ApplicationStart {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/repository/TagRepository.java
+++ src/main/java/com/example/repository/TagRepository.java
@@ -3,9 +3,11 @@ package com.example.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 
 import com.example.beans.Tags;
 
+@Component
 public interface TagRepository extends MongoRepository<Tags, Long> {
 
 	public List<Tags> findBytagName(String tagName);
